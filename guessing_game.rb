52fdb957ca6715c01faa977fed06b1f40e7929d1
David Bernheisel <david@bernheisel.com>
2015-09-01 12:51:49 -0400

    Initial commit. Normal mode

---
+++ guessing_game.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+
+def leave(message)
+  puts "#{message}"
+  exit
+end
+
+def guess_right?(guess, answer)
+  return true if guess == answer
+  return false
+end
+
+def is_integer?(n)
+  return true if n.to_i.to_s == n
+  return false
+end
+
+crystal = %q{
+                 _.---._
+            *  .'       '.  *
+           *  /  # #      \  *
+             | #           |
+          *  |             |  *
+              \       //  /
+           *   '.       .'  *
+             *   `'---'`  *
+                  )___(
+                 )_____(
+                (_______)
+}
+
+puts crystal
+puts "           NUMBER GUESSING GAME\n\n"
+
+
+# Determine which mode to play
+print "Who should guess the number? The computer or you?: "
+compguessresponse = gets.chomp!
+
+compguess = false
+case compguessresponse
+when "computer"
+  compguess = true
+when "you"
+  compguess = false
+when "me"
+  compguess = false
+else
+  leave("Not a valid response")
+end
+
+### COMPUTER GUESSING MODE
+# Setup
+guesses = []
+themagicnumber = (1..100).to_a.sample
+#puts themagicnumber
+
+while true
+  if guesses.length == 5
+    leave("You lose!")
+  end
+
+  print "Your guess: "
+  guess = gets.chomp!
+
+  if guess_right?(guess.to_i, themagicnumber)
+    puts "You win!"
+    break
+  end
+
+  if !is_integer?(guess)
+    puts "Dawg, we're guessing whole numbers, not baby names."
+    next
+  end
+
+  if guesses.include? guess
+    puts "Dawg, take some ginko. Try again."
+    next
+  end
+
+  guess.to_i > themagicnumber ? puts("You guessed too high.") : puts("You guessed too low.")
+  guesses << guess
+  #puts guesses
+end
